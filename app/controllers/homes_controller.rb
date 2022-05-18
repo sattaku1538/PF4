0d7dac734d6afbf74382a66521b2f585d0e53dc8
sattaku1538 <choose.chose.chosen@gmail.com>
2022-05-18 03:19:11 +0000

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,13 @@
 class HomesController < ApplicationController
+  
+  def top
+  end
+  
+  def index
+  end
+  
+  def about
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'index' => 'homes#index'
+  get 'about' => 'homes#about#'
+  
 end
